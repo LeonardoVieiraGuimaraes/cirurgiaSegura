53eab7220da88de5b3ada03d0e3fb6bf691a3d4f
LEONARDO VIEIRA GUIMARAES <leonardovieiraxy@hotmail.com>
2022-10-17 23:22:17 -0300

    Views e urls

--- cirurgiaSegura/checklist/views.py
+++ cirurgiaSegura/checklist/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
 
 # Create your views here.
+from django.http import HttpResponse
 
 
-class IndexView(TemplateView):
-    template_name = "index.html"
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
